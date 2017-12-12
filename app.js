6b46215d60cee6f9b67e6609eaa01288b2f5c446
dwilk1022 <dwilk1022@gmail.com>
2017-12-12 10:57:19 -0800

    git issues sorted out

---
+++ app.js
@@ -0,0 +1,130 @@
+var hours = ['7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm',];
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var Burien = {
+  name: '152nd st',
+  minCustsPerHour: 22,
+  maxCustsPerHour: 46,
+  avgCookiesPerCust: 7.6,
+  custsEachHour: [],
+  cookiesEachHour: [],
+  totalDailySales: 0,
+  calcCustsEachHour: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
+    }
+  },
+  calcCookiesEachHour: function(){
+    this.calcCustsEachHour();
+    for(var i = 0; i < hours.length; i++){
+      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
+      console.log(oneHour, 'one hour');
+      this.cookiesEachHour.push(oneHour);
+      this.totalDailySales += oneHour;
+      console.log(this.totalDailySales, 'total');
+    }
+  },
+}
+
+var Normandy Park = {
+  name: 'Manhattan Village',
+  minCustsPerHour: 33,
+  maxCustsPerHour: 78,
+  avgCookiesPerCust: 8.1,
+  custsEachHour: [],
+  cookiesEachHour: [],
+  totalDailySales: 0,
+  calcCustsEachHour: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
+    }
+  },
+  calcCookiesEachHour: function(){
+    this.calcCustsEachHour();
+    for(var i = 0; i < hours.length; i++){
+      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
+      console.log(oneHour, 'one hour');
+      this.cookiesEachHour.push(oneHour);
+      this.totalDailySales += oneHour;
+      console.log(this.totalDailySales, 'total');
+    }
+  },
+}
+
+var Lake Stevens = {
+  name: 'Frontier Village',
+  minCustsPerHour: 22,
+  maxCustsPerHour: 46,
+  avgCookiesPerCust: 7.6,
+  custsEachHour: [],
+  cookiesEachHour: [],
+  totalDailySales: 0,
+  calcCustsEachHour: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
+    }
+  },
+  calcCookiesEachHour: function(){
+    this.calcCustsEachHour();
+    for(var i = 0; i < hours.length; i++){
+      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
+      console.log(oneHour, 'one hour');
+      this.cookiesEachHour.push(oneHour);
+      this.totalDailySales += oneHour;
+      console.log(this.totalDailySales, 'total');
+    }
+  },
+}
+
+var Des Moins = {
+  name: '',
+  minCustsPerHour: 13,
+  maxCustsPerHour: 27,
+  avgCookiesPerCust: 5.3,
+  custsEachHour: [],
+  cookiesEachHour: [],
+  totalDailySales: 0,
+  calcCustsEachHour: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
+    }
+  },
+  calcCookiesEachHour: function(){
+    this.calcCustsEachHour();
+    for(var i = 0; i < hours.length; i++){
+      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
+      console.log(oneHour, 'one hour');
+      this.cookiesEachHour.push(oneHour);
+      this.totalDailySales += oneHour;
+      console.log(this.totalDailySales, 'total');
+    }
+  },
+}
+
+var Seattle = {
+  name: '4th Ave',
+  minCustsPerHour: 22,
+  maxCustsPerHour: 46,
+  avgCookiesPerCust: 7.6,
+  custsEachHour: [],
+  cookiesEachHour: [],
+  totalDailySales: 0,
+  calcCustsEachHour: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
+    }
+  },
+  calcCookiesEachHour: function(){
+    this.calcCustsEachHour();
+    for(var i = 0; i < hours.length; i++){
+      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
+      console.log(oneHour, 'one hour');
+      this.cookiesEachHour.push(oneHour);
+      this.totalDailySales += oneHour;
+      console.log(this.totalDailySales, 'total');
+    }
+  },
+}
