93945e447cab10d6db04f02f649182e399ccf2de
dwilk1022 <dwilk1022@gmail.com>
2017-12-13 09:56:01 -0800

    finished assignment had troubles with with the constructors  used lecture code as refrence to help me along

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var hours = ['7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm',];
+'use strict'
 
+var hours = ['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var Burien = {
-  name: '152nd st',
-  maxCustsPerHour: 46,
-  minCustsPerHour: 22,
-  avgCookiesPerCust: 7.6,
+var burien = {
+  name: '152nd St',
+  maxCustsPerHour: 25,
+  minCustsPerHour: 5,
+  avgCookiesPerCust: 10,
   custsEachHour: [],
   cookiesEachHour: [],
+  totalDailySales: 0,
   calcCustsEachHour: function(){
     for(var i = 0; i < hours.length; i++){
       this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
@@ -20,44 +22,33 @@ var Burien = {
     this.calcCustsEachHour();
     for(var i = 0; i < hours.length; i++){
       var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
-      console.log(oneHour, 'one hour');
-      this.cookiesEachHour.push(oneHour);
-      console.log(this.cookiesEachHour, 'total');
-    }
-  },
-}
-
-var Normandy Park = {
-  name: 'Manhattan Village',
-  maxCustsPerHour: 78,
-  minCustsPerHour: 33,
-  avgCookiesPerCust: 8.1,
-  custsEachHour: [],
-  cookiesEachHour: [],
-  calcCustsEachHour: function(){
-    for(var i = 0; i < hours.length; i++){
-      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
-    }
-  },
-  calcCookiesEachHour: function(){
-    this.calcCustsEachHour();
-    for(var i = 0; i < hours.length; i++){
-      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
-      console.log(oneHour, 'one hour');
       this.cookiesEachHour.push(oneHour);
       this.totalDailySales += oneHour;
-      console.log(this.totalDailySales, 'total');
     }
   },
-}
+  render: function(){
+    this.calcCookiesEachHour();
+    var ulEl = document.getElementById('152nd st');
+    for(var i = 0; i < hours.length; i++){
 
-var Lake Stevens = {
+      var liEl = document.createElement('li');
+       liEl.textContent = hours[i] + ':' + this.cookiesEachHour[i] + 'cookies';
+       ulEl.appendChild(liEl);
+      }
+      liEl = document.createElement('li');
+      liEl.textContent = 'Total:' + this.totalDailySales + 'cookies';
+      ulEl.appendChild(liEl);
+    }
+  }
+
+var lake stevens = {
   name: 'Frontier Village',
-  maxCustsPerHour: 46,
-  minCustsPerHour: 22,
-  avgCookiesPerCust: 7.6,
+  maxCustsPerHour: 35,
+  minCustsPerHour: 5,
+  avgCookiesPerCust: 7,
   custsEachHour: [],
   cookiesEachHour: [],
+  totalDailySales: 0,
   calcCustsEachHour: function(){
     for(var i = 0; i < hours.length; i++){
       this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
@@ -67,21 +58,33 @@ var Lake Stevens = {
     this.calcCustsEachHour();
     for(var i = 0; i < hours.length; i++){
       var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
-      console.log(oneHour, 'one hour');
       this.cookiesEachHour.push(oneHour);
       this.totalDailySales += oneHour;
-      console.log(this.totalDailySales, 'total');
     }
   },
+  render: function(){
+    this.calcCookiesEachHour();
+    var ulEl = document.getElementById('lake stevens');
+    for(var i = 0; i < hours.length; i++){
+
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ':' + this.cookiesEachHour[i] + 'cookies';
+      ulEl.appendChild(liEl);
+    }
+      liEl = document.createElement('li');
+      liEl.textContent = 'Total:' + this.totalDailySales + 'cookies';
+      ulEl.appendChild(liEl);
+  }
 }
 
-var Des Moins = {
-  name: 'Marina',
-  maxCustsPerHour: 27,
-  minCustsPerHour: 13,
-  avgCookiesPerCust: 5.3,
+var south park = {
+  name: 'Cloverdale',
+  maxCustsPerHour: 9,
+  minCustsPerHour: 37,
+  avgCookiesPerCust: 5.5,
   custsEachHour: [],
   cookiesEachHour: [],
+  totalDailySales: 0,
   calcCustsEachHour: function(){
     for(var i = 0; i < hours.length; i++){
       this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
@@ -91,33 +94,99 @@ var Des Moins = {
     this.calcCustsEachHour();
     for(var i = 0; i < hours.length; i++){
       var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
-      console.log(oneHour, 'one hour');
       this.cookiesEachHour.push(oneHour);
       this.totalDailySales += oneHour;
-      console.log(this.totalDailySales, 'total');
     }
   },
-}
+  render: function(){
+    this.calcCookiesEachHour();
+    var ulEl = document.getElementById('cloverdale');
+    for(var i = 0; i < hours.length; i++){
 
-var Seattle = {
-  name: '4th Ave',
-  maxCustsPerHour: 43,
-  minCustsPerHour: 55,
-  avgCookiesPerCust: 10.3,
-  custsEachHour: [],
-  cookiesEachHour: [],
-  calcCustsEachHour: function(){
-    for(var i = 0; i < hours.length; i++){
-      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
+    var liEl = document.createElement('li');
+     liEl.textContent = hours[i] +':' + this.cookiesEachHour[i] +'cookies';
+     ulEl.appendChild(liEl);
     }
-  },
-  calcCookiesEachHour: function(){
-    this.calcCustsEachHour();
-    for(var i = 0; i < hours.length; i++){
-      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
-      console.log(oneHour, 'one hour');
-      this.cookiesEachHour.push(oneHour);
-      console.log(this.cookiesEachHour, 'total');
+     liEl = document.createElement('li');
+     liEl.textContent = 'Total:' + this.totalDailySales +'cookies';
+     ulEl.appendChild(liEl);
     }
-  },
-}
+  }
+
+  var des moins = {
+    name: 'Marina',
+    maxCustsPerHour: 46,
+    minCustsPerHour: 8,
+    avgCookiesPerCust: 9,
+    custsEachHour: [],
+    cookiesEachHour: [],
+    totalDailySales: 0,
+    calcCustsEachHour: function(){
+      for(var i = 0; i < hours.length; i++){
+        this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
+      }
+    },
+    calcCookiesEachHour: function(){
+      this.calcCustsEachHour();
+      for(var i = 0; i < hours.length; i++){
+        var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
+        this.cookiesEachHour.push(oneHour);
+        this.totalDailySales += oneHour;
+      }
+    },
+    render: function(){
+      this.calcCookiesEachHour();
+      var ulEl = document.getElementById('marina');
+      for(var i = 0; i < hours.length; i++){
+
+        var liEl = document.createElement('li');
+         liEl.textContent = hours[i] + ':' + this.cookiesEachHour[i] + 'cookies';
+         ulEl.appendChild(liEl);
+        }
+        liEl = document.createElement('li');
+        liEl.textContent = 'Total:' + this.totalDailySales + 'cookies';
+        ulEl.appendChild(liEl);
+      }
+    }
+
+    var everett = {
+      name: 'Broadway',
+      maxCustsPerHour: 60,
+      minCustsPerHour: 19,
+      avgCookiesPerCust: 23,
+      custsEachHour: [],
+      cookiesEachHour: [],
+      totalDailySales: 0,
+      calcCustsEachHour: function(){
+        for(var i = 0; i < hours.length; i++){
+          this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
+        }
+      },
+      calcCookiesEachHour: function(){
+        this.calcCustsEachHour();
+        for(var i = 0; i < hours.length; i++){
+          var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
+          this.cookiesEachHour.push(oneHour);
+          this.totalDailySales += oneHour;
+        }
+      },
+      render: function(){
+        this.calcCookiesEachHour();
+        var ulEl = document.getElementById('broadway');
+        for(var i = 0; i < hours.length; i++){
+
+          var liEl = document.createElement('li');
+           liEl.textContent = hours[i] + ':' + this.cookiesEachHour[i] + 'cookies';
+           ulEl.appendChild(liEl);
+          }
+          liEl = document.createElement('li');
+          liEl.textContent = 'Total:' + this.totalDailySales + 'cookies';
+          ulEl.appendChild(liEl);
+        }
+      }
+
+burien.render();
+lakeStevens.render();
+southPark.render();
+desMoins.render();
+everett.render();
